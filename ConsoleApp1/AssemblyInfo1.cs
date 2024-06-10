5e68ad4331bd5442c594b874c9a9745d39646e89
Elydra555 <kenyer.zsiros01@gmail.com>
2024-06-10 15:17:43 +0200

    Add project files.

---
+++ ConsoleApp1/AssemblyInfo1.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("d0d423e8-e519-4ac5-aa7d-d0bd7f50f506")]
---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class2
+    {
+    public string Városnevek { get; set; }
+    public int Irányítószám { get; set; }
+
+    public int Kordináták { get; set; } 
+
+        public Városnevek(int id) { }
+        public Városnevek()
+        {
+            var a = sor.Split(";");
+            Városnevek = a[0];
+            Irányítószám = a[1];
+            Kordináták = a[2];
+        }
+        public override string? 
+        {
+            return $"Városnevek: {Városnevek}, Irányítószám: {Irányítószám}, Kordináták: {KoKordináták}";
+
+        
+
+
+
+
+
+    }
+}
